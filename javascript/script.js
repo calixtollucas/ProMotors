800d5b80e73f044effafaf2a207763fee1050061
Lucas Calixto <lucaslucenacalixto1@gmail.com>
2023-11-25 15:00:33 -0300

    Sistema de Cadastro concluído

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,47 @@
-
-//classes
+//CLASSES
 class Cliente{
-    #nome;
-    #sobrenome;
-    #sexo
-    #estado;
-    #cidade;
-    #email;
-    #senha;
+    nome;
+    sobrenome;
+    sexo
+    estado;
+    cidade;
+    email;
+    senha;
     constructor(nome, sobrenome, sexo, estado, cidade, email, senha){
-        this.#nome = nome,
-        this.#sobrenome = sobrenome,
-        this.#sexo = sexo,
-        this.#estado = estado,
-        this.#cidade = cidade,
-        this.#email = email,
-        this.#senha = senha
+        this.nome = nome,
+        this.sobrenome = sobrenome,
+        this.sexo = sexo,
+        this.estado = estado,
+        this.cidade = cidade,
+        this.email = email,
+        this.senha = senha
     }
 
     //getters and setters
-    get getNome(){
-        return this.#nome;
-    }
-    get getSobrenome(){
-        return this.#sobrenome;
-    }
-    get getSexo(){
-        return this.#sexo;
-    }
-    get getEstado(){
-        return this.#estado;
-    }
-    get getCidade(){
-        return this.#cidade;
-    }
-    get getEmail(){
-        return this.#email;
-    }
-    get getSenha(){
-        return this.#senha;
-    }
+    // get getNome(){
+    //     return this.#nome;
+    // }
+    // get getSobrenome(){
+    //     return this.#sobrenome;
+    // }
+    // get getSexo(){
+    //     return this.#sexo;
+    // }
+    // get getEstado(){
+    //     return this.#estado;
+    // }
+    // get getCidade(){
+    //     return this.#cidade;
+    // }
+    // get getEmail(){
+    //     return this.#email;
+    // }
+    // get getSenha(){
+    //     return this.#senha;
+    // }
 }
 const clientes = [];
+let clienteLogado;
 
 //--CADASTRO--
 //variáveis
@@ -55,10 +55,19 @@ const cadConSenha = document.querySelector('input#cadConSenha');
 const cadEstado = document.querySelector('select#cadEstado');
 const cadCidade = document.querySelector('input#cadCidade');
 
-cadForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
+function validation(){
+    let senhaMsg = document.querySelectorAll('span#senhaMsg');
+    let emailMsg = document.querySelector('span#emailMsg');
+    let validado = true;
 
-    let senhaMsg = document.querySelectorAll('span.senhaMsg');
+    if(!cadEmail.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailMsg.innerHTML = 'Por favor, digite um email válido';
+        setTimeout(()=>{
+            emailMsg.innerHTML = '';
+        },6000)
+
+        return false;
+    }
 
     if(cadSenha.value != cadConSenha.value){
             senhaMsg.forEach((element)=>{
@@ -67,9 +76,29 @@ cadForm.addEventListener('submit', (e)=>{
                     element.innerHTML = '';
                 },6000)
             })
-            alert('entrou!')
+
+            return false;
         }
-})
+
+        return true;
+}
+
+if(cadForm){
+    cadForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+
+        if(validation()){
+            alert('Cadastrado!');
+            clientes.push(new Cliente(cadNome.value, cadSobrenome.value, cadSexo.value, cadEstado.value, cadCidade.value, cadEmail.value, cadSenha.value))
+            console.log(clientes);
+            localStorage.setItem('users',JSON.stringify(clientes));
+            
+            window.location.href = 'login.html';
+        }
+
+    })
+}
+
 
 //--LOGIN--
 //variavéis
@@ -77,16 +106,27 @@ const formLog = document.querySelector('form#formLogin');
 const nomeUsuario = document.getElementById('loginUsu');
 const senhaUsuario = document.getElementById('loginSenha');
 
-formLog.addEventListener('submit', e =>{
-    e.preventDefault();
-    let nomeValue = nomeUsuario.value;
-    let senhaValue = senhaUsuario.value;
+if(formLog){
+    formLog.addEventListener('submit', e =>{
+        e.preventDefault();
+        let nomeValue = nomeUsuario.value;
+        let senhaValue = senhaUsuario.value;
 
-    clientes.forEach(element => { 
-        if((nomeValue == element.getNomeUsuario || nomeValue == element.getEmail) && senhaValue == element.getSenha){
-            alert('logado com sucesso!');
-        }
-    });
-
-    window.location.href = 'index.html';
-})
+        JSON.parse(localStorage.getItem('users')).forEach((element) =>{
+            
+            if(nomeValue == element.nome && senhaValue == element.senha){
+                clienteLogado = element;
+                localStorage.setItem('usuLogado', JSON.stringify(clienteLogado))
+                alert('Logado com sucesso!');
+                window.location.href = 'catalogo.html'
+                return;
+            } else {
+                document.querySelector('span#loginMsg').innerHTML = 'Usuário não encontrado ou você não digitou os dados corretamente, tente novamente ou crie uma conta.'
+                setTimeout(()=>{
+                    document.querySelector('span#loginMsg').innerHTML = '';
+                },4000)
+                return;
+            }
+        })
+    })
+}
